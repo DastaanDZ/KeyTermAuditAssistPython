d8bb9b13c0cdf6242141bce05ce4026972599f52
Baacha Adithya <85406431+adithyabaacha@users.noreply.github.com>
2025-12-26 16:10:48 +0530

    updated to system value

--- app/keyterm_entity.py
+++ app/keyterm_entity.py
@@ -5,7 +5,7 @@ class KeytermInsight(BaseModel):
     KEYTERM_INSIGHT_ID: int
     ORDER_NUMBER: int
     KEYTERM_CODE: str
-    SYSTEM_EDIT: str
+    SYSTEM_VALUE: str
     USER_VALUE: str 
     LAST_UPDATE_DATE: str
     LAST_UPDATE_BY: str
